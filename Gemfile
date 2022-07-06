0b0fc2b1b77e16620d695cdad32c9ec27859e494
Bryan Shears <brshears@gmail.com>
2022-07-06 15:43:07 -0600

    Third commit

--- Gemfile
+++ Gemfile
@@ -39,6 +39,10 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'pry'
+  gem 'rspec-rails'
+  gem 'capybara' #give us a way to use rspec to interact with the web pages the way a user would.
+  gem 'launchy' #save_and_open_page
+  gem 'simplecov'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/songs', to: 'songs#index'
   #get '/songs' is accepting the route or request from the browser, and then what do we do with it?
-  #we route it to the songs controller & the index action. 
+  #we route it to the songs controller & the index action.
+  get '/songs/:id', to: 'songs#show'
 end
